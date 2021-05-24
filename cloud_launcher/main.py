bbe5ee42828cf472f2ad315381f83305092f5800
Moises Perez <moises.perez@grupoica.com>
2021-05-24 19:47:08 +0200

    added main for the launcher app

--- cloud_launcher/main.py
+++ cloud_launcher/main.py
@@ -1,5 +1,13 @@
+from cloud_handler.adapters import AwsEc2AMILauncher
+from frontend_handlers.adapters import ApiFrontendAdapter
+from launcher import Launcher
 
 if __name__ == '__main__':
-    pass
+    aws_ec2_ami_adapter = AwsEc2AMILauncher()
+    launcher = Launcher(aws_ec2_ami_adapter)
+    api = ApiFrontendAdapter()
+    api.add_launch_app_callback(launcher.launch_app)
+    api.add_get_launch_status_callback(launcher.check_app_launch)
+    api.run()
 
 
